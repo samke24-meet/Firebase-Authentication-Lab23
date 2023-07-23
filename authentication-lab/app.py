af3a85fb4c7406838acb79fd6e9d8ee30e00e525
samke24-meet <bokutosam0@gmail.com>
2023-07-23 18:26:26 +0300

    still working on it

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,12 +2,30 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {
+  "apiKey": "AIzaSyAG81cxeR_E7vYaamDZT2fmArx3OGMgVCg",
+  "authDomain": "dinosaurcake-ba4e5.firebaseapp.com",
+  "projectId": "dinosaurcake-ba4e5",
+  "storageBucket": "dinosaurcake-ba4e5.appspot.com",
+  "messagingSenderId": "466473887088",
+  "appId": "1:466473887088:web:b19c5b649fcfe41116dbb5",
+  "databaseURL": ""
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error=""
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+        login_session['user'] = auth.create_user_with
+
     return render_template("signin.html")
 
 
